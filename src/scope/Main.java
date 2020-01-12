b6f3d0b14868163529f8d47050b8bdb2912d3322
kaungmyatpaing21 <kaungmyatpaing2110@gmail.com>
2020-01-13 00:09:15 +0630

    scope challenge

--- src/scope/Main.java
+++ src/scope/Main.java
@@ -1,14 +1,20 @@
 package scope;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        ScopeCheck scopeCheck = new ScopeCheck();
-        scopeCheck.timesTwo();
+//        ScopeCheck scopeCheck = new ScopeCheck();
+//        scopeCheck.timesTwo();
+//
+//        ScopeCheck.InnerClass innerClass = scopeCheck.new InnerClass();
+//        innerClass.timesTwo();
+//
+//        // visibility test
+//        scopeCheck.useInner();
 
-        ScopeCheck.InnerClass innerClass = scopeCheck.new InnerClass();
-        innerClass.timesTwo();
-
-        // visibility test
-        scopeCheck.useInner();
+        //chellenge
+        X x = new X(new Scanner(System.in));
+        x.x();
     }
 }
